e4cf5fa2d1426bac873cee6cc3e97e8287f7cd95
CJ Horlebein <chrishorlebein@gmail.com>
2018-09-20 21:01:20 -0400

    initial commit

---
+++ marinaraSauce.js
@@ -0,0 +1,14 @@
+let two = 2;
+let oneForth = "1/4"
+
+function marinaraRecipe(cookName){
+    console.log(`hey ${cookName}! This is my recipe!`);
+    console.log("- Heat a medium-large saucepan over medium heat");
+    console.log("- Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about " + two + " minutes");
+    console.log("- Add " + oneForth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("- Stir in " + oneForth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+
+marinaraRecipe("CJ");
